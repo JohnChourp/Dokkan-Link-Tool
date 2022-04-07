cfe867bf41b28d1091f6d413e586ffa995afc240
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2022-04-07 23:44:57 +0300

    improved filterCategory

--- JS/filterCategory.js
+++ JS/filterCategory.js
@@ -9,9 +9,9 @@ function filterCategory(){
 	
 	let categories = document.getElementsByClassName("categories");
 	let categoriesBtn = document.getElementsByClassName("categories-btn");
-	let appeared = document.getElementsByClassName("appeared");
 	let appearedType = document.getElementsByClassName("appearedType");
 	let appearedRarity = document.getElementsByClassName("appearedRarity");
+	let appearedClass = document.getElementsByClassName("appearedClass");
 	
 	for(let i = 0; i < categoriesBtn.length; i ++){
 		catList[i] = document.getElementsByClassName(categoriesBtn.item(i).id);
@@ -33,28 +33,26 @@ function filterCategory(){
 	
 	for(let j = 0; j < categoriesOrange.length; j ++){
 		categoriesOrange.item(j).addEventListener('click' , function(){
-			if(categoriesOrange.item(j).classList.contains("activeCategory")){
-				categoriesOrange.item(j).classList.remove("activeCategory");
-				
-				for(let i = 0; i < categories.length; i ++){
-					categories.item(i).style.display = "none";
-					categories.item(i).classList.remove("appeared");
-				}
-				
-				for(let j = 0; j < categoriesBtn.length; j ++){
-					if(categoriesBtn.item(j).classList.contains("activeCategory")){
-						for(let i = 0; i < catList[j].length; i ++){
+			if(appearedType.length < 1 && appearedClass.length < 1 && appearedRarity.length < 1){
+				if(categoriesOrange.item(j).classList.contains("activeCategory")){
+					categoriesOrange.item(j).classList.remove("activeCategory");
+					
+					for(let i = 0; i < categories.length; i ++){
+						categories.item(i).style.display = "none";
+						categories.item(i).classList.remove("appeared");
+					}
+					
+					for(let j = 0; j < categoriesBtn.length; j ++){
+						if(categoriesBtn.item(j).classList.contains("activeCategory")){
 							showActiveCategoryWithDelay(catList , j);
 						}
 					}
+				}else{
+					categoriesOrange.item(j).classList.add("activeCategory");
+					showActiveCategoryWithDelay(catList , j);
 				}
-			}else{
-				categoriesOrange.item(j).classList.add("activeCategory");
-				showActiveCategoryWithDelay(catList , j);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesOrange , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -79,9 +77,7 @@ function filterCategory(){
 				categoriesGreen.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesGreen , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -106,9 +102,7 @@ function filterCategory(){
 				categoriesYellow.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesYellow , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -133,9 +127,7 @@ function filterCategory(){
 				categoriesCyan.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length + categoriesCyan.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesCyan , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -160,9 +152,7 @@ function filterCategory(){
 				categoriesBlue.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length + categoriesCyan.length + categoriesCyan.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesBlue , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -187,9 +177,7 @@ function filterCategory(){
 				categoriesPurple.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length + categoriesCyan.length + categoriesCyan.length + categoriesBlue.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesPurple , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -214,9 +202,7 @@ function filterCategory(){
 				categoriesRed.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length + categoriesCyan.length + categoriesCyan.length + categoriesBlue.length + categoriesPurple.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesRed , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -241,9 +227,7 @@ function filterCategory(){
 				categoriesDarkGreen.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length + categoriesCyan.length + categoriesCyan.length + categoriesBlue.length + categoriesPurple.length + categoriesRed.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesDarkGreen , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 	
@@ -268,30 +252,50 @@ function filterCategory(){
 				categoriesDarkBlue.item(j).classList.add("activeCategory");
 				showActiveCategoryWithDelay(catList , j + categoriesOrange.length + categoriesGreen.length + categoriesCyan.length + categoriesCyan.length + categoriesBlue.length + categoriesPurple.length + categoriesRed.length + categoriesDarkGreen.length);
 			}
-			if(appeared.length > 0 && appearedRarity.length < 1){
-				filterInit(appearedType , appearedRarity , categories , categoriesDarkBlue , j)
-			}
+			filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesGreen , j);
 		});
 	}
 }
 
-function filterInit(appearedType , appearedRarity , categories , categoriesColor , j){
-	if(appearedType.length < 1 && appearedRarity.length > 0){
-		filterAppearRarity(categories , categoriesColor , j);
-	}
-	
-	if(appearedType.length > 0 && appearedRarity.length < 1){
+function filterInit(appearedType , appearedRarity , appearedClass , categories , categoriesColor , j){
+	if(appearedType.length > 0 && appearedClass.length < 1 && appearedRarity.length < 1){
 		filterAppearType(categories , categoriesColor , j);
 	}
 	
-	if(appearedType.length > 0 && appearedRarity.length > 0){
+	if(appearedType.length < 1 && appearedClass.length < 1 && appearedRarity.length > 0){
+		filterAppearRarity(categories , categoriesColor , j);
+	}
+	
+	if(appearedType.length < 1 && appearedClass.length > 0 && appearedRarity.length < 1){
+		filterAppearClass(categories , categoriesColor , j);
+	}
+	
+	if(appearedType.length > 0 && appearedClass.length < 1 && appearedRarity.length > 0){
 		filterAppearTypeAndAppearRarity(categories , categoriesColor , j);
+	}
+	
+	if(appearedType.length > 0 && appearedClass.length > 0 && appearedRarity.length < 1){
+		filterAppearTypeAndAppearClass(categories , categoriesColor , j);
+	}
+	
+	if(appearedType.length < 1 && appearedClass.length > 0 && appearedRarity.length > 0){
+		filterAppearRarityAndAppearClass(categories , categoriesColor , j);
 	}
 }
 
 function filterAppearRarity(categories , categoriesColor , j){
 	for(let i = 0; i < categories.length; i ++){
 		if(categories.item(i).classList.contains("appeared") && categories.item(i).classList.contains("appearedRarity") && categories.item(i).classList.contains(categoriesColor.item(j).id)){
+			categories.item(i).style.display = "inline-block";
+		}else{
+			categories.item(i).style.display = "none";
+		}
+	}
+}
+
+function filterAppearClass(categories , categoriesColor , j){
+	for(let i = 0; i < categories.length; i ++){
+		if(categories.item(i).classList.contains("appeared") && categories.item(i).classList.contains("appearedClass") && categories.item(i).classList.contains(categoriesColor.item(j).id)){
 			categories.item(i).style.display = "inline-block";
 		}else{
 			categories.item(i).style.display = "none";
@@ -312,6 +316,26 @@ function filterAppearType(categories , categoriesColor , j){
 function filterAppearTypeAndAppearRarity(categories , categoriesColor , j){
 	for(let i = 0; i < categories.length; i ++){
 		if(categories.item(i).classList.contains("appeared") && categories.item(i).classList.contains("appearedType") && categories.item(i).classList.contains("appearedRarity") && categories.item(i).classList.contains(categoriesColor.item(j).id)){
+			categories.item(i).style.display = "inline-block";
+		}else{
+			categories.item(i).style.display = "none";
+		}
+	}
+}
+
+function filterAppearRarityAndAppearClass(categories , categoriesColor , j){
+	for(let i = 0; i < categories.length; i ++){
+		if(categories.item(i).classList.contains("appeared") && categories.item(i).classList.contains("appearedRarity") && categories.item(i).classList.contains("appearedClass") && categories.item(i).classList.contains(categoriesColor.item(j).id)){
+			categories.item(i).style.display = "inline-block";
+		}else{
+			categories.item(i).style.display = "none";
+		}
+	}
+}
+
+function filterAppearTypeAndAppearClass(categories , categoriesColor , j){
+	for(let i = 0; i < categories.length; i ++){
+		if(categories.item(i).classList.contains("appeared") && categories.item(i).classList.contains("appearedType") && categories.item(i).classList.contains("appearedClass") && categories.item(i).classList.contains(categoriesColor.item(j).id)){
 			categories.item(i).style.display = "inline-block";
 		}else{
 			categories.item(i).style.display = "none";
